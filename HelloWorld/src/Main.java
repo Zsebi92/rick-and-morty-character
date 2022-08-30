55b9654d813711364449f24a6f8c38a7d5a197a9
Zsebi92 <59623854+Zsebi92@users.noreply.github.com>
2022-08-30 14:19:23 +0200

    ein fancy feature

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -26,6 +26,7 @@ public class Main {
         System.out.println("Ein Weiterer Commit!");
 
         System.out.println("Ein Fancy Feature");
+        System.out.println("Ein Fancy Feature");
 
     }
 }
